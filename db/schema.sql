669d8b922585230ea13d7cba14727e2876807429
Philip Digiglio <37520782+PhilipDig@users.noreply.github.com>
2018-08-19 10:21:38 -0700

    Updated schema for heroku jawsdb deployment

--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,10 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE v6b2sdfeidr9asrq;
 
 CREATE TABLE burgers
 (
 	id int NOT NULL AUTO_INCREMENT,
 	burger_name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
+    createdAt timestamp default current_timestamp,
 	PRIMARY KEY (id)
 );
